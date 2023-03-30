0873dd6403d9f46737211abcfdb3ecc16f37ed0d
uhammgm <uhammgm@stu.ccsdut.org>
2023-03-30 11:27:35 -0600

    did crap

--- flukynum.py
+++ flukynum.py
@@ -1,7 +1,10 @@
 import random
-
+import time
 t = 0
+l = 0
+start = time.time()
 while t <= 7:
+    
     for n in range(1, 10000):
         FT = 0
         for factor in range(1, n):
@@ -11,5 +14,10 @@ while t <= 7:
         if n == FT:
             print(str(n) + " is a flucky number.")
         t += 1
+   
+end = time.time()
+timer = end - start
+print("It took " + str(round(timer, 2)) + " seconds")
+print("Total loops: " + "7")
 
    
